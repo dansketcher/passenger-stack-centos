601484768d1265c41130dc8e739b2a2e35b29a30
Dan Sketcher <dan@sketcher.net>
2010-03-05 16:37:53 +1000

    Correct repo installation

--- config/stack/repos.rb
+++ config/stack/repos.rb
@@ -1,9 +1,7 @@
 package :yum_repository_pgdg do
   version 'pgdg-centos-8.4-1'
   
-  source "http://yum.pgsqlrpms.org/reporpms/8.4/#{version}.noarch.rpm" do
-    custom_install "sudo rpm -ivh #{version}.noarch.rpm"
-  end
+  rpm "http://yum.pgsqlrpms.org/reporpms/8.4/#{version}.noarch.rpm"
 
   verify do
     has_rpm version
@@ -14,9 +12,7 @@ package :yum_repository_rpmforge do
   version 'rpmforge-release-0.5.1-1.el5.rf'
   arch = 'x86_64'
   
-  source "http://dag.wieers.com/rpm/packages/rpmforge-release/#{version}.#{arch}.rpm" do
-    custom_install "sudo rpm -ivh #{version}.#{arch}.rpm"
-  end
+  rpm "http://dag.wieers.com/rpm/packages/rpmforge-release/#{version}.#{arch}.rpm"
 
   verify do
     has_rpm version
@@ -26,9 +22,7 @@ end
 package :yum_repository_epel do
   version 'epel-release-5-3'
   
-  source "http://download.fedora.redhat.com/pub/epel/5/i386/#{version}.noarch.rpm" do
-    custom_install "sudo rpm -ivh #{version}.noarch.rpm"
-  end
+  rpm "http://download.fedora.redhat.com/pub/epel/5/i386/#{version}.noarch.rpm"
 
   verify do
     has_rpm version
