58797b8a9cd4b8d2a6bf6a1bdacb9ca902f90184
Dan Sketcher <dan@sketcher.net>
2010-03-06 21:51:44 +1000

    Add dependency

--- config/stack/ntp.rb
+++ config/stack/ntp.rb
@@ -5,7 +5,10 @@ package :ntp do
     post :install, 'sudo ln -fs /usr/share/zoneinfo/UTC /etc/localtime'
     post :install, 'sudo /usr/sbin/ntpdate -u -s 0.centos.pool.ntp.org 1.centos.pool.ntp.org 2.centos.pool.ntp.org && sudo /sbin/hwclock -w'
   end
+  
   verify do
     has_executable '/usr/sbin/ntpd'
   end
+  
+  requires :build_essential
 end
