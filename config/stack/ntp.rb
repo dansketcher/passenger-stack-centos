70623d52481c42bb1707347732ca7cf749936434
Dan Sketcher <dan@sketcher.net>
2010-03-08 11:48:25 +1000

    Correct path error

--- config/stack/ntp.rb
+++ config/stack/ntp.rb
@@ -3,7 +3,8 @@ package :ntp do
   yum 'ntp' do
     post :install, 'sudo /sbin/service ntpd start'
     post :install, 'sudo ln -fs /usr/share/zoneinfo/UTC /etc/localtime'
-    post :install, 'sudo /usr/sbin/ntpdate -u -s 0.centos.pool.ntp.org 1.centos.pool.ntp.org 2.centos.pool.ntp.org && sudo /sbin/hwclock -w'
+    post :install, 'sudo /usr/sbin/ntpdate -u -s 0.centos.pool.ntp.org 1.centos.pool.ntp.org 2.centos.pool.ntp.org'
+    post :install, 'sudo /sbin/hwclock -w'
   end
   
   verify do
