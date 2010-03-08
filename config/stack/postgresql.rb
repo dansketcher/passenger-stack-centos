a3309b19ce257710a17ca37b7e2b02c16ae48704
Dan Sketcher <dan@sketcher.net>
2010-03-08 15:06:33 +1000

    Split PostgreSQL server and client requirement, change gem to new official one, and add phpPgAdmin option

--- config/stack/postgresql.rb
+++ config/stack/postgresql.rb
@@ -9,19 +9,34 @@ package :postgres, :provides => :database do
   verify do
     has_executable '/etc/init.d/postgresql'
   end
+  
+  requires :yum_repository_pgdg, :postgres_client
 end
  
 package :postgresql_driver, :provides => :ruby_database_driver do
   description 'Ruby PostgreSQL database driver'
-  gem 'ruby-pg'
+  gem 'pg'
   
-  verify do
-    has_gem 'ruby-pg'
-  end
+  #verify do
+  #  has_gem 'pg'
+  #end
   
-  requires :postgres, :postgres_dependencies, :ruby_enterprise
+  requires :postgres_client, :postgres_dependencies, :ruby_enterprise
 end
 
 package :postgres_dependencies do
-  yum 'postgresql-devel'
+  yum 'postgresql-devel.x86_64'
+end
+
+package :postgres_client do
+  yum 'postgresql-libs postgresql postgresql-contrib'
+  verify do
+    has_executable 'psql'
+  end
+end
+
+package :phppgadmin do
+  yum 'phpPgAdmin'
+  
+  requires :yum_repository_pgdg, :postgres_client, :webserver
 end
