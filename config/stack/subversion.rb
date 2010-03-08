1d9c7b4030523c204cfb1bf207c8be1711a618c8
Dan Sketcher <dan@sketcher.net>
2010-03-08 16:15:11 +1000

    Package for mod_dav_svn

--- config/stack/subversion.rb
+++ config/stack/subversion.rb
@@ -5,3 +5,13 @@ package :subversion, :provides => :scm do
     has_executable 'svn'
   end
 end
+
+package :apache_mod_dav_svn do
+  yum "mod_dav_svn"
+
+  verify do
+    has_file '/etc/httpd/conf.d/subversion.conf'
+  end
+
+  requires :apache
+end
