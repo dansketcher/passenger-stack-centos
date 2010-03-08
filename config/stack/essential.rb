742592609df845f872c6167c874dba8ec882d58d
Dan Sketcher <dan@sketcher.net>
2010-03-08 12:31:05 +1000

    Always run yum-updatesd service

--- config/stack/essential.rb
+++ config/stack/essential.rb
@@ -2,5 +2,6 @@ package :build_essential do
   description 'Build tools'
   yum 'build-essential' do
     pre :install, 'yum -y update'
+    pre :install, 'sudo /sbin/service yum-updatesd start'
   end
 end
